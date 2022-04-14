aefaa939f977f5e5d46bb79757c73c2d3031d6a1
Vince Lauffer <vlauffer@gmail.com>
2022-04-14 18:57:28 -0400

    added base to dates

--- app/api/dates.js
+++ app/api/dates.js
@@ -23,13 +23,15 @@ router.get('/',(req,res)=>{
 
 
     var finalQuery =`
-    SELECT emoji, ucode, CAST(COUNT(ucode) AS VARCHAR(64)) AS count, created_at 
-    FROM (SELECT emojis.emoji as emoji, emojis.ucode, messages.created_at as created_at 
+    SELECT sub.emoji, sub.ucode, CAST(COUNT(sub.ucode) AS VARCHAR(64)) AS count, created_at, 
+    LEFT(emoji_images.base, LENGTH(emoji_images.base ) -1) AS base 
+    FROM (SELECT emojis.emoji, emojis.ucode, messages.created_at as created_at 
         FROM emojis INNER JOIN messages on emojis.message_id=messages.message_id 
         WHERE created_at BETWEEN ? AND ? UNION ALL 
-        SELECT reactions.emoji as emoji, reactions.ucode, reactions.created_at as created_at 
-        FROM reactions WHERE created_at BETWEEN ? AND ? ) sub1 
-        GROUP BY DAY(created_at), ucode ;
+        SELECT reactions.emoji, reactions.ucode, reactions.created_at as created_at 
+        FROM reactions WHERE created_at BETWEEN ? AND ? ) sub 
+        LEFT JOIN emoji_images ON sub.ucode=emoji_images.ucode
+        GROUP BY DAY(created_at), sub.ucode ;
 
     `;
 
--- app/emojis/EmojiManager.js
+++ app/emojis/EmojiManager.js
@@ -37,7 +37,7 @@ class EmojiManager{
         // var query = `SELECT sub.emoji, CAST(COUNT(sub.emoji) AS VARCHAR(64)) AS count, emoji_images.base          
         // FROM (SELECT emoji FROM emojis UNION ALL SELECT emoji from reactions) as sub LEFT JOIN emoji_images 
         // ON sub.emoji=emoji_images.emoji GROUP BY sub.emoji;`;
-        var query = `SELECT sub.emoji, sub.ucode, CAST(COUNT(sub.emoji) AS VARCHAR(64)) AS count, LEFT(emoji_images.base, LENGTH(emoji_images.base ) -1)
+        var query = `SELECT sub.emoji, sub.ucode, CAST(COUNT(sub.emoji) AS VARCHAR(64)) AS count, LEFT(emoji_images.base, LENGTH(emoji_images.base ) -1) AS base
             FROM (SELECT emoji, ucode FROM emojis UNION ALL SELECT emoji, ucode from reactions) as sub LEFT JOIN emoji_images
             ON sub.ucode=emoji_images.ucode GROUP BY sub.emoji;`;
         // var query = format(`SELECT emoji, CAST(COUNT(emoji) AS VARCHAR(64)) AS count, emoji_images.base 
@@ -106,7 +106,7 @@ class EmojiManager{
         // ;`;
 
         var query = `SELECT sub.uid, discord_users.username, sub.emoji, sub.ucode, CAST(COUNT(sub.ucode) AS VARCHAR(64)) AS count, 
-        LEFT(emoji_images.base, LENGTH(emoji_images.base ) -1) 
+        LEFT(emoji_images.base, LENGTH(emoji_images.base ) -1) AS base
         FROM (SELECT emojis.emoji AS emoji, emojis.ucode, messages.user_id AS uid, messages.message_id AS mid FROM emojis
             INNER JOIN messages ON emojis.message_id=messages.message_id UNION ALL 
             SELECT reactions.emoji as emoji, reactions.ucode, reactions.user_id as uid, reactions.message_id as mid FROM reactions) as sub 
